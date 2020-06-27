3f7103fa931190cf5b781d03b1c626524c69179b
Thiago Muniz <tmtic@hotmail.com>
2020-06-27 20:05:29 -0300

    Adicionando Bootstrap

--- views/home.php
+++ views/home.php
@@ -1,37 +1,42 @@
 <div class="container">
-	<table border="0">
-	<thead>
-	<tr>
-		<th>ID</th>
-		<th>ITEM</th>
-	</tr>
-	</thead>
-	<tbody>
-	<?php foreach ($lista as $item):?>
-	<tr>
-		<td><?=$item->id;?></td>
-		<td><?=$item->title;?></td>
-	</tr>
-	<?php endforeach; ?>
-	</tbody>
+	<table class='table table-md table-hover table-bordered '>
+		<thead class="thead-light">
+			<tr>
+				<th>#</th>
+				<th>ITEM</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($lista as $item):?>
+				<tr>
+					<td><?=$item->id;?></td>
+					<td><?=$item->title;?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
 	</table>
-
-	<?php if($paginaAtual > 1): ?>
-	<a href="<?=BASE_URL;?>?p=1">Início</a>
-	<a href="<?=BASE_URL;?>?p=<?=($paginaAtual-1)?>">&lt;</a>
-	<?php endif; ?>
-
-	<?php for($q=1; $q <= $paginas; $q++): ?>
-		<?php if($paginaAtual == $q): ?>
-			<a href="<?=BASE_URL;?>?p=<?=$q?>"><span class='active'><?=$q;?></span></a>
-		<?php else: ?>
-			<a href="<?=BASE_URL;?>?p=<?=$q?>"><?=$q;?></a>
-		<?php endif; ?>
-	<?php endfor; ?>
-
-
-	<?php if($paginaAtual != $paginas): ?>
-	<a href="<?=BASE_URL;?>?p=<?=($paginaAtual+1)?>">&gt;</a>	
-	<a href="<?=BASE_URL;?>?p=<?=$paginas;?>">Última</a>
-	<?php endif; ?>
 </div>	
+
+	<nav aria-label="Page navigation">
+	  <ul class="pagination justify-content-center">
+	    <li class="page-item">
+		<?php if($paginaAtual > 1): ?>
+			<a class="page-link" href="<?=BASE_URL;?>?p=<?=($paginaAtual-1)?>">Voltar</a>
+		<?php endif; ?>	    	
+	      
+	    </li>
+			<?php for($q=1; $q <= $paginas; $q++): ?> 
+				<?php if($paginaAtual == $q): ?>
+					<li class="page-item active"><a class="page-link" aria-current="page" href="<?=BASE_URL;?>?p=<?=$q?>"><?=$q;?></a></li>
+				<?php else: ?>
+					<li class="page-item"><a class="page-link" href="<?=BASE_URL;?>?p=<?=$q?>"><?=$q;?></a></li>
+				<?php endif; ?>
+			<?php endfor; ?>	    
+	    <li class="page-item">
+			<?php if($paginaAtual != $paginas): ?>
+			<a class="page-link" href="<?=BASE_URL;?>?p=<?=($paginaAtual+1)?>">Avançar</a>
+			<?php endif; ?>	    	
+	      
+	    </li>
+	  </ul>
+	</nav>	
